c2cefa5d0b1d296f3fcb90adc522d2e3bef4eff6
Donovan Rost <drost@ucsc.edu>
2018-02-14 17:03:15 -0800

    users can now log out

--- index.php
+++ index.php
@@ -1,34 +1,12 @@
 <?php
 include("/Users/Donovan/Sites/sn/classes/DB.php");
+include("/Users/Donovan/Sites/sn/classes/Login.php");
 
 
-function isLoggedIn(){
-	if(isset($_COOKIE['SNID'])){
-		if(DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token'=>sha1($_COOKIE['SNID'])))){
-			$userid = DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token'=>sha1($_COOKIE['SNID'])))[0]['user_id'];
-			
-			if(isset($_COOKIE['SNID_'])){
-				return $userid;
 
-			}
-			else{
-				DB::query('INSERT INTO login_tokens VALUES (NULL, :token, :user_id)', array(':token' =>sha1($token), ':user_id' =>$userid));
-				DB::query('DELETE FROM login_tokens WHERE token=:token', array(':token' =>sha1($_COOKIE['SNID'])));
-
-				setcookie("SNID", $token, time() + 60*60*24*7, '/', NULL, NULL, TRUE);
- 				setcookie("SNID_", '1', time() + 60*60*24*3, '/', NULL, NULL, TRUE);
-
- 				return $userid;
-
-			}
-		}
-	}
-
-	return false;
-}
-
-if(isLoggedIn()){
+if(Login::isLoggedIn()){
 	echo 'Logged In';
+	//echo Login::isLoggedIn();
 } 
 else { echo 'Not logged in';}
 
