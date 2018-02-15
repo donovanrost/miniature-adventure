84e8cae5f8db64f21fa50b5a398fac8d6d337c4c
Donovan Rost <drost@ucsc.edu>
2018-02-14 16:06:00 -0800

    login.php will check log in/out

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ include("/Users/Donovan/Sites/sn/classes/DB.php");
 
 function isLoggedIn(){
 	if(isset($_COOKIE['SNID'])){
-		if(DB::query('SELECT user_id FROM login_tokens WHERE token = :token'), array(':token'=>sha1($_COOKIE['SNID']))){
-			DB::query('SELECT user_id FROM login_tokens WHERE token = :token'), array(':token'=>sha1($_COOKIE['SNID']))[0]['user_id'];
-			return $user_id;
+		if(DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token'=>sha1($_COOKIE['SNID'])))){
+			$userid = DB::query('SELECT user_id FROM login_tokens WHERE token = :token', array(':token'=>sha1($_COOKIE['SNID'])))[0]['user_id'];
+			return $userid;
 		}
 	}
 
 	return false;
 }
 
-if(isLoggedin()){
+if(isLoggedIn()){
 	echo 'Logged In';
 } 
 else { echo 'Not logged in';}
--- login.php
+++ login.php
@@ -19,6 +19,8 @@ include("/Users/Donovan/Sites/sn/classes/DB.php");
  			$user_id = DB::query('SELECT id FROM users WHERE username = :username', array(':username'=>$username))[0]['id'];
  			DB::query('INSERT INTO login_tokens VALUES (NULL, :token, :user_id)', array(':token' =>sha1($token), ':user_id' =>$user_id));
 
+ 			setcookie("SNID", $token, time() + 60*60*24*7, '/', NULL, NULL, TRUE);
+
  		}
  		else{echo 'Incorrect password';}
 
